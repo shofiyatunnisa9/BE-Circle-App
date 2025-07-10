1436db753dbd0dd7f281da734f8d902c43409729
shofiyatunnisa9 <shofiyatunnisa939@gmail.com>
2025-07-10 10:58:58 +0700

    fix : deploy backend cors

--- src/configs/cors.ts
+++ src/configs/cors.ts
@@ -1,7 +1,7 @@
 import cors from "cors";
 
 const corsMiddleware = cors({
-  origin: "http://localhost:5173, https://circle-app-fe-roan.vercel.app",
+  origin: ["http://localhost:5173", "https://circle-app-fe-roan.vercel.app"],
   credentials: true,
 });
 
